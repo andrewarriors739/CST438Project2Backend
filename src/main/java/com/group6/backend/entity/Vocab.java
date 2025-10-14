533f04ba4a9960e00871b0ecd6dca1e648aba543
JZavala210 <juanczavala22@gmail.com>
2025-10-14 13:45:49 -0700

    Fix Vocab entity compile errors

--- src/main/java/com/group6/backend/entity/Vocab.java
+++ src/main/java/com/group6/backend/entity/Vocab.java
@@ -1,66 +1,63 @@
 package com.group6.backend.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import java.time.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-//This entity file will represent a table we have in our database 
-// it will also communicate with Hibernate to create the schema for our database
-
-//Word table defined in entity file
 @Entity
-@Table(name == "words")
-
-//We then will define what a word will consist off and what it will include
+@Table(name = "words")
 public class Vocab {
 
-    //The word will need an ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    //We will need to make sure that the word is a String and not left blank
-    //We also need to set the size of the String for the word
+    @NotBlank
+    @Size(max = 255)
+    private String word;
 
-    //We also need to make sure that the definition of the word is a string and not blank
-    //We also need to set the size of the String for the definition
+    @NotBlank
+    @Size(max = 1000)
+    private String definition;
 
+    // Constructors
+    public Vocab() {}
 
-    //Constructors
-    public Word(){
-
-    }
-
-    public Word(String word, String definition){
+    public Vocab(String word, String definition) {
         this.word = word;
         this.definition = definition;
     }
 
-    //Getters and Setters
-    public Long getID(){
+    // Getters and Setters
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getWord(){
+    public String getWord() {
         return word;
     }
 
-    public void setWord(String word, String definition){
+    public void setWord(String word) {
         this.word = word;
     }
 
-    public String getDefinition(){
+    public String getDefinition() {
         return definition;
     }
 
-    public void setDefinition(){
-         this.definition = definition;
+    public void setDefinition(String definition) {
+        this.definition = definition;
     }
-
-
-
 }
+
 
 
 
